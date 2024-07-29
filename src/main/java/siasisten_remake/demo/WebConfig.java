3f033bee83dfe80e30b1a12f5397c80fa201ecc0
Rizki Maulana <rizki.maulana23@ui.ac.id>
2024-07-29 21:27:12 +0700

    A little bit update of base template I guess...

--- src/main/java/siasisten_remake/demo/WebConfig.java
+++ src/main/java/siasisten_remake/demo/WebConfig.java
@@ -2,7 +2,6 @@ package siasisten_remake.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import siasisten_remake.demo.interceptor.LoginInterceptor;
 
@@ -11,11 +10,12 @@ public class WebConfig  implements WebMvcConfigurer {
     @Autowired
     private LoginInterceptor loginInterceptor;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns("/login")
-                .excludePathPatterns("/register");
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns("/login")
+//                .excludePathPatterns("/register")
+//                .excludePathPatterns("/static/**");
+//    }
 }
